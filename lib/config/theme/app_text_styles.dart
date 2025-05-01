c19c90cbdb6b712a61afa8d25943fbe7e7759387
hj9023 <hy9458@duksung.ac.kr>
2025-05-01 23:25:59 +0900

    🧹 Chore: themes에 텍스트 스타일 상수 추가

---
+++ lib/config/theme/app_text_styles.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class AppTextStyles {
+  static const TextStyle display1 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 40,
+    color: AppColors.neutralBlack,
+    height: 1.4,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle display1Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 40,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+
+  static const TextStyle heading1 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 34,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading1Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 34,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading2 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 28,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading2Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 28,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading3 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 22,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading3Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 22,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading4 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 20,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading4Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 20,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+
+  static const TextStyle body1 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 17,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body1Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 17,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body2 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 16,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body2Bold = TextStyle(
+    color: AppColors.neutralBlack,
+    fontFamily: 'PretendardVariable',
+    fontSize: 16,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body3 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 15,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body3Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 15,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body4 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 13,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body4Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 13,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body5 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 12,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body5Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 12,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body6 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body6Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+
+  // color: white
+  static const TextStyle display1White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 40,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle display1BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 40,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+
+  static const TextStyle heading1White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 34,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading1BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 34,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading2White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 28,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading2BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 28,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading3White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 22,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading3BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 22,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle heading4White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 20,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle heading4BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 20,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+
+  static const TextStyle body1White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 17,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body1BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 17,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body2White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 16,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body2BoldWhite = TextStyle(
+    color: AppColors.neutralWhite,
+    fontFamily: 'PretendardVariable',
+    fontSize: 16,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body3White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 15,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body3BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 15,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body4White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 13,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body4BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 13,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body5White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 12,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body5BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 12,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static const TextStyle body6White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static const TextStyle body6BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+}
