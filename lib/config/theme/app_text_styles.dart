c009dfdb1d5d2b3bac5757064efe330cb957c2fd
hj9023 <hy9458@duksung.ac.kr>
2025-05-02 15:48:55 +0900

    ♻️ Refactor: 생성자 구조 개선 및 fontSize 단위 sp로 정리 - indicator에서 사용하는 단위 변경: .w -> .r - button radius 적용 - textStyle 상수 사용하도록 수정

--- lib/config/theme/app_text_styles.dart
+++ lib/config/theme/app_text_styles.dart
@@ -1,276 +1,278 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app_colors.dart';
 
 class AppTextStyles {
-  static const TextStyle display1 = TextStyle(
+  static final TextStyle display1 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 40,
+    fontSize: 40.sp,
     color: AppColors.neutralBlack,
     height: 1.4,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle display1Bold = TextStyle(
+  static final TextStyle display1Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 40,
+    fontSize: 40.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
 
-  static const TextStyle heading1 = TextStyle(
+  static final TextStyle heading1 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 34,
+    fontSize: 34.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading1Bold = TextStyle(
+  static final TextStyle heading1Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 34,
+    fontSize: 34.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading2 = TextStyle(
+  static final TextStyle heading2 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 28,
+    fontSize: 28.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading2Bold = TextStyle(
+  static final TextStyle heading2Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 28,
+    fontSize: 28.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading3 = TextStyle(
+  static final TextStyle heading3 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 22,
+    fontSize: 22.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading3Bold = TextStyle(
+  static final TextStyle heading3Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 22,
+    fontSize: 22.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading4 = TextStyle(
+  static final TextStyle heading4 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 20,
+    fontSize: 20.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading4Bold = TextStyle(
+  static final TextStyle heading4Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 20,
+    fontSize: 20.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
 
-  static const TextStyle body1 = TextStyle(
+  static final TextStyle body1 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 17,
+    fontSize: 17.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body1Bold = TextStyle(
+  static final TextStyle body1Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 17,
+    fontSize: 17.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body2 = TextStyle(
+  static final TextStyle body2 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 16,
+    fontSize: 16.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body2Bold = TextStyle(
+  static final TextStyle body2Bold = TextStyle(
     color: AppColors.neutralBlack,
     fontFamily: 'PretendardVariable',
-    fontSize: 16,
+    fontSize: 16.sp,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body3 = TextStyle(
+  static final TextStyle body3 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 15,
+    fontSize: 15.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body3Bold = TextStyle(
+  static final TextStyle body3Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 15,
-    color: AppColors.neutralBlack,
-    fontVariations: <FontVariation>[FontVariation('wght', 600)],
-  );
-  static const TextStyle body4 = TextStyle(
-    fontFamily: 'PretendardVariable',
-    fontSize: 13,
-    color: AppColors.neutralBlack,
-    fontVariations: <FontVariation>[FontVariation('wght', 400)],
-  );
-  static const TextStyle body4Bold = TextStyle(
-    fontFamily: 'PretendardVariable',
-    fontSize: 13,
+    fontSize: 15.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body5 = TextStyle(
+  static final TextStyle body4 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 12,
+    fontSize: 13.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body5Bold = TextStyle(
+  static final TextStyle body4Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 12,
+    fontSize: 13.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body6 = TextStyle(
+  static final TextStyle body5 = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 11,
+    fontSize: 12.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body6Bold = TextStyle(
+  static final TextStyle body5Bold = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 11,
+    fontSize: 12.sp,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static final TextStyle body6 = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11.sp,
+    color: AppColors.neutralBlack,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static final TextStyle body6Bold = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11.sp,
     color: AppColors.neutralBlack,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
 
   // color: white
-  static const TextStyle display1White = TextStyle(
+  static final TextStyle display1White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 40,
+    fontSize: 40.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle display1BoldWhite = TextStyle(
+  static final TextStyle display1BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 40,
+    fontSize: 40.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
 
-  static const TextStyle heading1White = TextStyle(
+  static final TextStyle heading1White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 34,
+    fontSize: 34.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading1BoldWhite = TextStyle(
+  static final TextStyle heading1BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 34,
+    fontSize: 34.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading2White = TextStyle(
+  static final TextStyle heading2White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 28,
+    fontSize: 28.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading2BoldWhite = TextStyle(
+  static final TextStyle heading2BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 28,
+    fontSize: 28.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading3White = TextStyle(
+  static final TextStyle heading3White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 22,
+    fontSize: 22.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading3BoldWhite = TextStyle(
+  static final TextStyle heading3BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 22,
+    fontSize: 22.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle heading4White = TextStyle(
+  static final TextStyle heading4White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 20,
+    fontSize: 20.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle heading4BoldWhite = TextStyle(
+  static final TextStyle heading4BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 20,
+    fontSize: 20.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
 
-  static const TextStyle body1White = TextStyle(
+  // fontSize: 17.sp
+  static final TextStyle body1White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 17,
+    fontSize: 17.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body1BoldWhite = TextStyle(
+  static final TextStyle body1BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 17,
+    fontSize: 17.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body2White = TextStyle(
+  static final TextStyle body2White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 16,
+    fontSize: 16.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body2BoldWhite = TextStyle(
+  static final TextStyle body2BoldWhite = TextStyle(
     color: AppColors.neutralWhite,
     fontFamily: 'PretendardVariable',
-    fontSize: 16,
+    fontSize: 16.sp,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body3White = TextStyle(
+  static final TextStyle body3White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 15,
+    fontSize: 15.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body3BoldWhite = TextStyle(
+  static final TextStyle body3BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 15,
-    color: AppColors.neutralWhite,
-    fontVariations: <FontVariation>[FontVariation('wght', 600)],
-  );
-  static const TextStyle body4White = TextStyle(
-    fontFamily: 'PretendardVariable',
-    fontSize: 13,
-    color: AppColors.neutralWhite,
-    fontVariations: <FontVariation>[FontVariation('wght', 400)],
-  );
-  static const TextStyle body4BoldWhite = TextStyle(
-    fontFamily: 'PretendardVariable',
-    fontSize: 13,
+    fontSize: 15.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body5White = TextStyle(
+  static final TextStyle body4White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 12,
+    fontSize: 13.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body5BoldWhite = TextStyle(
+  static final TextStyle body4BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 12,
+    fontSize: 13.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
-  static const TextStyle body6White = TextStyle(
+  static final TextStyle body5White = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 11,
+    fontSize: 12.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 400)],
   );
-  static const TextStyle body6BoldWhite = TextStyle(
+  static final TextStyle body5BoldWhite = TextStyle(
     fontFamily: 'PretendardVariable',
-    fontSize: 11,
+    fontSize: 12.sp,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 600)],
+  );
+  static final TextStyle body6White = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11.sp,
+    color: AppColors.neutralWhite,
+    fontVariations: <FontVariation>[FontVariation('wght', 400)],
+  );
+  static final TextStyle body6BoldWhite = TextStyle(
+    fontFamily: 'PretendardVariable',
+    fontSize: 11.sp,
     color: AppColors.neutralWhite,
     fontVariations: <FontVariation>[FontVariation('wght', 600)],
   );
--- lib/features/training_common/presentation/pages/onboarding_recording_guide_page.dart
+++ lib/features/training_common/presentation/pages/onboarding_recording_guide_page.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sync2sing/config/theme/app_colors.dart';
-import 'package:sync2sing/features/training_common/presentation/widgets/onboarding_page_indicator.dart';
+import 'package:sync2sing/config/theme/app_text_styles.dart';
 import 'package:sync2sing/features/training_common/presentation/widgets/youtube_player_widget.dart';
 import 'package:sync2sing/shared/providers/watch_youtube_providers.dart';
+import 'package:sync2sing/shared/widgets/page_indicator.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sync2sing/config/routes/route_names.dart';
@@ -17,7 +18,7 @@ class OnboardingRecordingGuidePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     ScreenUtil.init(context, designSize: const Size(390, 844));
 
-    final bool isButtonEnabled = ref.watch(
+    final bool _isButtonEnabled = ref.watch(
       isOnboardingRecordingStartButtonEnabledProvider,
     );
 
@@ -35,7 +36,7 @@ class OnboardingRecordingGuidePage extends ConsumerWidget {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  OnboardingPageIndicator(currentPage: 4),
+                  PageIndicator(currentPage: 4, pageCount: 6), // 상단 페이지네이션 위젯
                   Container(
                     margin: EdgeInsets.fromLTRB(0, 30.h, 0, 20.h),
                     width: 327.w,
@@ -49,55 +50,43 @@ class OnboardingRecordingGuidePage extends ConsumerWidget {
                           child: Text(
                             "마지막 과정이에요",
                             textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 22.w,
-                              color: AppColors.neutralBlack,
-                              fontVariations: <FontVariation>[
-                                FontVariation('wght', 600),
-                              ],
-                              height: 1.4,
-                            ),
+                            style: AppTextStyles.heading3Bold,
                           ),
                         ),
 
                         Text(
                           "아래 음원을 듣고 \n후렴구를 똑같이 따라 불러주세요",
                           textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 20.w,
-                            color: AppColors.neutralBlack,
-                            fontVariations: <FontVariation>[
-                              FontVariation('wght', 400),
-                            ],
-                            height: 1.4,
-                          ),
+                          style: AppTextStyles.heading4,
                         ),
                       ],
                     ),
                   ),
+                  // 유튜브 영상
                   Container(
                     width: 327.w,
                     margin: EdgeInsets.symmetric(vertical: 20.w),
                     child: YoutubePlayerWidget(
                       videoId: 'Qy9cj-zwbVY', // 도레미송 공식 가사 비디오
-                      flags: YoutubePlayerFlags(
-                        autoPlay: false,
-                        startAt: 42,
-                      ), // do a deer 부분에서 영상 시작
+                      // do a deer 부분에서 영상 시작, 자동 재생 금지
+                      flags: YoutubePlayerFlags(autoPlay: false, startAt: 42),
                     ),
                   ),
+                  // 시작하기 버튼
                   Container(
                     width: double.infinity,
                     height: 50.w,
-                    margin: EdgeInsets.fromLTRB(0, 30.h, 0, 10.h),
+                    margin: EdgeInsets.fromLTRB(0, 30.h, 0, 30.h),
                     alignment: Alignment(0.0, 0.0),
                     child: CupertinoButton(
                       color: AppColors.primaryPink,
                       disabledColor: Color(0xFFF8D6DA),
+                      borderRadius: BorderRadius.circular(10.r),
                       minSize: 0.0,
                       padding: EdgeInsets.all(0),
+                      // 버튼 활성화 조건을 만족하면 onboardingRecordingSong 페이지로 이동 / 아니면 비활성화 상태
                       onPressed:
-                          isButtonEnabled // 버튼 활성화 조건을 만족하면 onboardingRecordingSong 페이지로 이동 / 아니면 비활성화 상태
+                          _isButtonEnabled
                               ? () {
                                 context.goNamed(
                                   AppRouteNames.onboardingRecordingSong,
@@ -107,17 +96,10 @@ class OnboardingRecordingGuidePage extends ConsumerWidget {
                       child: Center(
                         child: Text(
                           "시작하기",
-                          style: TextStyle(
-                            fontSize: 17.w,
-                            color: AppColors.neutralWhite,
-                            fontVariations: <FontVariation>[
-                              FontVariation(
-                                'wght',
-                                isButtonEnabled ? 600 : 400,
-                              ),
-                            ],
-                            // height: 1.4,
-                          ),
+                          style:
+                              _isButtonEnabled
+                                  ? AppTextStyles.body1BoldWhite
+                                  : AppTextStyles.body1White,
                         ),
                       ),
                     ),
