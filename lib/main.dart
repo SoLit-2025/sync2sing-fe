eaaeeb4a22917c7d9b66044b20bee40614d7599e
KMJ <minj20@duksung.ac.kr>
2025-05-04 04:31:33 +0900

    ✨ Feat: MaximumPitchPage 구현 #9

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'config/routes/go_router_provider.dart';
 import 'config/theme/app_theme.dart';
 
@@ -13,10 +14,19 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final router = ref.watch(goRouterProvider);
-    return MaterialApp.router(
-      routerConfig: router,
-      title: "sync2sing",
-      theme: appTheme,
+
+    // ScreenUtilInit으로 앱 전체를 감쌈 (반응형)
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp.router(
+          routerConfig: router,
+          title: "sync2sing",
+          theme: appTheme,
+        );
+      },
     );
   }
 }
