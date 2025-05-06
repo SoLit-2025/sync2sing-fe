af71aac289c2948de9a61a09a2bc6f493de35215
KMJ <minj20@duksung.ac.kr>
2025-05-06 18:00:50 +0900

    ♻️ Refactor: Clip.hardEdge를 적용하여 연도 선택 박스 테두리 개선 #6

--- lib/features/onboarding/presentation/pages/user_birth_info_input_page.dart
+++ lib/features/onboarding/presentation/pages/user_birth_info_input_page.dart
@@ -134,15 +134,17 @@ class _UserBirthInfoInputPageState extends State<UserBirthInfoInputPage> {
                   GestureDetector(
                     onTap: _toggleYearPicker,
                     child: Container(
-                      width: 326.w,
-                      height: 50.h,
+                      width: (326.w).roundToDouble(),
+                      height: (50.h).roundToDouble(),
                       padding: EdgeInsets.symmetric(horizontal: 16.w),
+                      clipBehavior: Clip.hardEdge, // 여기에 추가!
                       decoration: BoxDecoration(
                         color: AppColors.neutralWhite,
                         borderRadius: BorderRadius.circular(10.r),
                         border: Border.all(
                           color: AppColors.neutralMediumGray,
-                          width: 1.w,
+                          width: 1.0,
+                          strokeAlign: BorderSide.strokeAlignInside,
                         ),
                       ),
                       alignment: Alignment.center,
