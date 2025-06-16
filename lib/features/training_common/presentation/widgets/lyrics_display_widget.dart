a721a19b6c040df89f1f088845be607b88b2c197
hj9023 <hy9458@duksung.ac.kr>
2025-06-16 13:45:50 +0900

    🐞 Fix: doremi 가사를 원본에 맞게 수정 및 불필요한 json 파일 삭제

--- lib/features/training_common/presentation/widgets/lyrics_display_widget.dart
+++ lib/features/training_common/presentation/widgets/lyrics_display_widget.dart
@@ -13,7 +13,7 @@ class _LyricsSectionState extends State<LyricsSection> {
   @override
   Widget build(BuildContext context) {
     return Text(
-      "(전주중) \nDO - a deer, \na female deer \nRE - a drop of golden sun",
+      "(전주중) \nDoe - a deer, \na female deer \nRay - a drop of golden sun",
       style: AppTextStyles.heading1Bold.copyWith(color: AppColors.grayscale3),
       textAlign: TextAlign.center,
     );
