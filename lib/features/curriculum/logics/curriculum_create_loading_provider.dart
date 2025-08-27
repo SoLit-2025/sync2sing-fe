d81b1994dabc8c4ebb6b72a505c6a538d225e01a
hj9023 <hy9458@duksung.ac.kr>
2025-08-27 15:32:39 +0900

    🔥 Remove: 불필요한 provider 삭제 #75

--- lib/features/curriculum/logics/curriculum_create_loading_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final curriculumCreateLoadingProvider = FutureProvider.autoDispose<void>((ref) async {
-  await Future.delayed(const Duration(seconds: 2)); // 2초 지연
-});
